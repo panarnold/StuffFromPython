e3f5a8e62830ba9d2960f5021551c93fce36861a
Arnold Cytrowski <cytrowskiarnold@gmail.com>
2020-10-06 17:00:13 +0200

    matrix_image.py is fully working

--- matrix_image.py
+++ matrix_image.py
@@ -8,3 +8,20 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', '0', '0', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
+#6 rows, 8 collumns
+
+def print_stuff(stuff):
+    image = ''
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            image+= stuff[j][i]
+        image += '\n'
+    print(image)
+
+print_stuff(grid)
+
+
+
+
+    
+
